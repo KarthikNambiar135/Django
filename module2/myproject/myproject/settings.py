16df8493baa4275fbf0a9e1fe8fa970a438f9d17
KarthikNambiar135 <karthiknambiar135@gmail.com>
2025-03-04 16:31:29 +0530

    Error Handling

--- module2/myproject/myproject/settings.py
+++ module2/myproject/myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jlko6&y2##4o8l31yd9bxqoyj-vih_hu8bmgk@wd(pu4k9zf#='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

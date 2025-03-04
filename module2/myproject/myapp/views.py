e4124552175a92b5a6e7eb0cc1421145e739ac91
KarthikNambiar135 <karthiknambiar135@gmail.com>
2025-03-04 14:34:13 +0530

    Changes made to urls.py and views.py in myapp to demonstrate request parameter in the user defined function inside the views.py(myapp) and used certain constants to be rendered into the Html Repsonse.

--- module2/myproject/myapp/views.py
+++ module2/myproject/myapp/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def home(response):
-    return HttpResponse("Welcome to the homnepage of LittleLemons!")
+# def home(response):
+#     return HttpResponse("Welcome to the homnepage of LittleLemons!")
+
+def home(request):
+    path = request.path
+    scheme = request.scheme
+    method = request.method
+    address = request.META['REMOTE_ADDR']
+    user_agent = request.META['HTTP_USER_AGENT']
+    path_info = request.path_info
+    
+    msg = f"""<br>
+        <br>Path: {path}
+        <br>Address: {address}
+        <br>Method: {method}
+        <br>Scheme: {scheme}
+        <br>User Agent: {user_agent}
+        <br>Path Info: {path_info}
+        <br><br>
+    """
+    # response = HttpResponse("This showcases a success result, 200!")
+    return HttpResponse(msg, content_type = 'text/html', charset='utf-8')
+    # return response
